217f4509f70be2ef9ffbf73e4de198a8963e14df
Aline <alinepascal@gmail.com>
2020-06-11 21:53:52 +0200

    git tag step-2

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -14,13 +14,3 @@ function capitalizeFirstLetters(string) {
 //console.log(capitalizeFirstLetters("la première lettre doit être en majuscule"))
 
 module.exports = capitalizeFirstLetters;
-
-
-// Check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirstLetters(''), '');
-// Check that it works for a 1-word string
-assert.strictEqual(capitalizeFirstLetters('zozo'), 'Zozo');
-// Check that it works for a string
-assert.strictEqual(capitalizeFirstLetters('zozo is in the zoo'), 'Zozo Is In The Zoo');
